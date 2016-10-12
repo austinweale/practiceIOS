43c7ca4d46780298b7afc6720f740150404b15d9
Austin Weale <austinweale@gmail.com>
2016-10-11 19:21:29 -0700

    initialized project

---
+++ myApp/myApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myApp
+//
+//  Created by Austin Weale on 10/11/16.
+//  Copyright © 2016 Austin Weale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
